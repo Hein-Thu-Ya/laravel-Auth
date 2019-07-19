44b5de9c65a33291baf33bf8be98adb67c457fd5
Hein Thu Ya <heinthuya12@gmail.com>
2019-07-20 00:39:17 +0630

    update route on web.php file

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 
 Route::get('/', 'AuthController@index')->middleware('checkUserLogin');
 
-Route::get('/login', 'AuthController@login')->name('auth.showLogin')->middleware('checkUserNotLogin');
-Route::get('/register', 'AuthController@register')->name('auth.showRegister')->middleware('checkUserNotLogin');
+Route::group(['middleware' => ['checkUserNotLogin']], function(){
+    Route::get('/login', 'AuthController@login')->name('auth.showLogin');
+    Route::get('/register', 'AuthController@register')->name('auth.showRegister');
 
-Route::post('/register', 'AuthController@postRegister')->name('auth.postRegister')->middleware('checkUserNotLogin');
-Route::post('/login', 'AuthController@postLogin')->name('auth.postLogin')->middleware('checkUserNotLogin');
+    Route::post('/register', 'AuthController@postRegister')->name('auth.postRegister');
+    Route::post('/login', 'AuthController@postLogin')->name('auth.postLogin');
+});
+
 Route::get('/logout', 'AuthController@logout')->name('auth.logout');
